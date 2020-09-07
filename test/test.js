90b86b7a050fc25468704acea024c31c26920747
Michael Chang <mchang0926@gmail.com>
2020-09-07 13:49:25 -0700

    updating unit test for search

--- test/test.js
+++ test/test.js
@@ -57,7 +57,7 @@ describe("Field test for spliting description string into two.", () => {
         assert.deepEqual(search.chopDesc(sampleDesc2).split("\n\n").length, 2)
     })
 
-    let sampleDesc3 = 'Basic laws of probability, sample distributions. Introduction to statistical inference and applications.  Prerequisite: or Corequisite: MATH 152 or 155 or 158. Students wishing an intuitive appreciation of a broad range of statistical strategies may wish to take STAT 100 first.   Quantitative.'
+    let sampleDesc3 = 'Basic laws of probability, sample distributions. Introduction to statistical inference and applications.  Prerequisite: or **Corequisite:** MATH 152 or 155 or 158. Students wishing an intuitive appreciation of a broad range of statistical strategies may wish to take STAT 100 first.   Quantitative.'
     it("Split STAT 270 information into 2.", () => {
         assert.deepEqual(search.chopDesc(sampleDesc3).split("\n\n").length, 2)
     })
@@ -110,19 +110,39 @@ describe("Field test for checking course number.", () => {
 })
 
 
+describe("Field test for formatting WQB array", () => {
+    let WQB1 = ["Q", "B-Sci"]
+    let answer1 = "Q, B-Sci"
+    it("Check if the string will look like this: Q, B-Sci", () => {
+        assert.deepEqual(search.WQBFormat(WQB1), answer1)
+    })
+
+    let WQB2 = ["Q"]
+    let answer2 = "Q"
+    it("Check if the string will look like this: Q", () => {
+        assert.deepEqual(search.WQBFormat(WQB2), answer2)
+    })
+})
+
+
 // Desc: testing the function findClass
 describe("Field test for finding course information (Main function)", () => {
 
+    // initialize current term
+    let currTerm = search.getDate()
+
     // Check cmpt 295
     let class1 = ["cmpt", "295"]
-    let answer1 = "CMPT 295 - Introduction to Computer Systems\n\nThe curriculum introduces students to topics in computer architecture that are considered fundamental to an understanding of the digital systems underpinnings of computer systems.  \n\nPrerequisite: Either (MACM 101 and ((CMPT 125 and CMPT 127) or CMPT 135)) or (MATH 151 and CMPT 102 for students in an Applied Physics program).   Students with credits for CMPT 150 or 250 may not take this course for further credit. \nCredits: 3"
+    let url1 = `http://www.sfu.ca/students/calendar/${currTerm[1]}/${currTerm[0]}/courses/${class1[0]}/${class1[1]}.html`
+    let answer1 = `**CMPT 295** - Introduction to Computer Systems\n\nThe curriculum introduces students to topics in computer architecture that are considered fundamental to an understanding of the digital systems underpinnings of computer systems.  \n\n**Prerequisite:** Either (MACM 101 and ((CMPT 125 and CMPT 127) or CMPT 135)) or (MATH 151 and CMPT 102 for students in an Applied Physics program).   Students with credits for CMPT 150 or 250 may not take this course for further credit.\n**Credits:** 3\n\n**Link:** ${url1}`
     it("Check if it can search CMPT 295.", () => {
         assert.deepEqual(search.findClass(class1), answer1)
     })
 
     // Check stat 843
     let class2 = ["stat", "843"]
-    let answer2 = "STAT 843 - Functional Data Analysis\n\nAn introduction to smoothing and modelling of functional data. Basis expansion methods, functional\nregression models and derivative estimation are covered. \n\nPrerequisite: STAT 830 or permission of the instructor. \nCredits: 4"
+    let url2 = `http://www.sfu.ca/students/calendar/${currTerm[1]}/${currTerm[0]}/courses/${class2[0]}/${class2[1]}.html`
+    let answer2 = `**STAT 843** - Functional Data Analysis\n\nAn introduction to smoothing and modelling of functional data. Basis expansion methods, functional\nregression models and derivative estimation are covered. \n\n**Prerequisite:** STAT 830 or permission of the instructor.\n**Credits:** 4\n\n**Link:** ${url2}`
     it("Check if it can search STAT 843.", () => {
         assert.deepEqual(search.findClass(class2), answer2)
     })
@@ -143,23 +163,26 @@ describe("Field test for finding course information (Main function)", () => {
 
     // Check cmpt 376w
     let class5 = ["cmpt", "376w"]
-    let answer5 = "CMPT 376W - Technical Writing and Group Dynamics\n\nCovers professional writing in computing science, including format conventions and technical reports. Examines group dynamics, including team leadership, dispute resolution and collaborative writing. Also covers research methods. \n\nPrerequisite: CMPT 275 or CMPT 276.   Students with credit for CMPT 376 may not take this course for further credit. Writing. \nCredits: 3\nWQB: W"
+    let url5 = `http://www.sfu.ca/students/calendar/${currTerm[1]}/${currTerm[0]}/courses/${class5[0]}/${class5[1]}.html`
+    let answer5 = `**CMPT 376W** - Technical Writing and Group Dynamics\n\nCovers professional writing in computing science, including format conventions and technical reports. Examines group dynamics, including team leadership, dispute resolution and collaborative writing. Also covers research methods. \n\n**Prerequisite:** CMPT 275 or CMPT 276.   Students with credit for CMPT 376 may not take this course for further credit. Writing.\n**Credits:** 3\n**WQB:** W\n\n**Link:** ${url5}`
     it("Check if it can search CMPT 376W (Class with WQB).", () => {
         assert.deepEqual(search.findClass(class5), answer5)
     })
 
     // Check hist 102w class with WQB
     let class6 = ["hist", "102w"]
-    let answer6 = "HIST 102W - Canada since Confederation\n\nCanadian social, political, and economic history from 1867, examining aboriginal/settler relations, immigration, regionalism, foreign policy, economic development, culture, and political movements. Students with credit for HIST 102 may not take this course for further credit. Writing/Breadth-Humanities.\nCredits: 3\nWQB: W,B-Hum"
+    let url6 = `http://www.sfu.ca/students/calendar/${currTerm[1]}/${currTerm[0]}/courses/${class6[0]}/${class6[1]}.html`
+    let answer6 = `**HIST 102W** - Canada since Confederation\n\nCanadian social, political, and economic history from 1867, examining aboriginal/settler relations, immigration, regionalism, foreign policy, economic development, culture, and political movements. Students with credit for HIST 102 may not take this course for further credit. Writing/Breadth-Humanities.\n**Credits:** 3\n**WQB:** W, B-Hum\n\n**Link:** ${url6}`
     it("Check if it can search HIST 102W (Class with WQB).", () => {
         assert.deepEqual(search.findClass(class6), answer6)
     })
 
     // Check educ 902b
     let class7 = ["educ", "902b"]
-    let answer7 = "EDUC 902B - Interdisciplinary Seminar in Contemporary Educational Theory B\n\nA further consideration of concepts explored in the EDUC 902 \"A\" course, with a view to providing students with opportunities to apply these ideas within their own educational settings. \n\nCorequisite: EDUC 902A.\nCredits: 3"
+    let url7 = `http://www.sfu.ca/students/calendar/${currTerm[1]}/${currTerm[0]}/courses/${class7[0]}/${class7[1]}.html`
+    let answer7 = `**EDUC 902B** - Interdisciplinary Seminar in Contemporary Educational Theory B\n\nA further consideration of concepts explored in the EDUC 902 \"A\" course, with a view to providing students with opportunities to apply these ideas within their own educational settings. \n\n**Corequisite:** EDUC 902A.\n**Credits:** 3\n\n**Link:** ${url7}`
     it("Check if it can search EDUC 902B.", () => {
-        assert.deepEqual(search.findClass(class6), answer6)
+        assert.deepEqual(search.findClass(class7), answer7)
     })
 
     // check if its incomplete
